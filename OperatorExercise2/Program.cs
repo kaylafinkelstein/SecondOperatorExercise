4b40f0f9006f299c826d5d93008fa99f8eab33c3
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-04-11 00:25:12 -0400

    operator second exercise

---
+++ OperatorExercise2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+            
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            Console.WriteLine("What is the radius of your circle?");
+#pragma warning disable CS8604 // Possible null reference argument.
+            var radius = double.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possible null reference argument.
+
+            var calculateArea = AreaOfCircle(radius);
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {calculateArea}");
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
